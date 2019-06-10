69cc9f8e091ebaf0db689dc06600f86580d7caab
SagiDana <sagidana1@gmail.com>
2019-06-10 16:47:28 +0300

    fixing evil-indent-plus

--- emacs/init.el
+++ emacs/init.el
@@ -49,9 +49,10 @@
     (evil-leader/set-key "sx" 'helm-cscope-find-this-symbol-no-prompt)
     ;; List all references inside current function
     (evil-leader/set-key "sX" 'helm-cscope-find-called-function-no-prompt)
-
     ;; Fold all by level
     (evil-leader/set-key "zl" 'hs-hide-level)
+    ;; Git status
+    (evil-leader/set-key "gs" 'magit-status)
   )
 
   (use-package evil-surround
@@ -62,8 +63,17 @@
   (use-package evil-magit
     :ensure t)
 
-  (use-package evil-indent-textobject
-    :ensure t))
+  (use-package evil-indent-plus
+    :ensure t
+    :config
+    (define-key evil-inner-text-objects-map "i" 'evil-indent-plus-i-indent)
+    (define-key evil-outer-text-objects-map "i" 'evil-indent-plus-a-indent)
+    (define-key evil-inner-text-objects-map "I" 'evil-indent-plus-i-indent-up)
+    (define-key evil-outer-text-objects-map "I" 'evil-indent-plus-a-indent-up)
+    (define-key evil-inner-text-objects-map "J" 'evil-indent-plus-i-indent-up-down)
+    (define-key evil-outer-text-objects-map "J" 'evil-indent-plus-a-indent-up-down)
+  )
+)
 
 ;; Ranger 
 (use-package ranger :ensure t
@@ -83,6 +93,13 @@
 (use-package helm-cscope
   :ensure t
   )
+
+;; -----------------------------
+;; Programming Languages Support
+;; -----------------------------
+;; C# support
+(use-package csharp-mode
+  :ensure t)
 
 ;; PHP support
 (use-package php-mode 
@@ -109,6 +126,7 @@
 (add-hook 'c-mode-common-hook #'hs-minor-mode)
 (add-hook 'python-mode-hook #'hs-minor-mode)
 (add-hook 'emacs-lisp-mode-hook #'hs-minor-mode)
+(add-hook 'csharp-mode-hook #'hs-minor-mode)
 
 ;; Enable cscope 
 ;;   apt-get install cscope
@@ -118,6 +136,7 @@
 ;;   cscope -R 
 (add-hook 'c-mode-common-hook 'helm-cscope-mode)
 (add-hook 'python-mode-hook 'helm-cscope-mode)
+(add-hook 'csharp-mode-hook 'helm-cscope-mode)
 
 ;; Auto-generated
 (custom-set-variables
@@ -128,7 +147,7 @@
  '(helm-follow-mode-persistent t)
  '(package-selected-packages
    (quote
-    (helm-projectile helm-ag-r helm-ag ranger helm spacemacs-theme use-package powerline php-mode evil-surround evil-leader evil-indent-textobject elpy))))
+    (evil-indent-plus helm-projectile helm-ag-r helm-ag ranger helm spacemacs-theme use-package powerline php-mode evil-surround evil-leader elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
