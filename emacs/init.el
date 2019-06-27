0bfbfca3b389b446fc05f35f0c8554398b7af2cf
SagiDana <sagidana1@gmail.com>
2019-06-27 11:40:06 -0700

    configure org mode

--- emacs/init.el
+++ emacs/init.el
@@ -41,6 +41,25 @@
 ;; replace M-x with helm-M-x
 (global-set-key (kbd "M-x") 'helm-M-x)
 
+;; Org-Mode
+(use-package org
+  :ensure t
+  :config
+  ;; Hide markers like *bold* and /italic/. 
+  (setq org-hide-emphasis-markers t)
+
+  ;; Change lists created by '-' to '•''
+  (font-lock-add-keywords 'org-mode
+                        '(("^ *\\([+-]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Use bullets instead of asterisks (the defaults)
+  (use-package org-bullets
+    :ensure t
+    :config
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  )
+
 ;; Evil-Mode
 (use-package evil
   :ensure t
@@ -250,7 +269,7 @@
 (use-package helm-projectile
   :ensure t
   :config
-  ;;(setq projectile-project-search-path '("/Projects"))
+  (setq projectile-project-search-path '("/Projects"))
   (projectile-discover-projects-in-search-path)
   )
 
@@ -347,10 +366,19 @@
  '(helm-follow-mode-persistent t)
  '(package-selected-packages
    (quote
-    (jedi websocket sr-speedbar treemacs swiper counsel imenu-list evil-indent-plus helm-projectile helm-ag-r helm-ag ranger helm spacemacs-theme use-package powerline php-mode evil-surround evil-leader elpy))))
+    (org-evil evil-org jedi websocket sr-speedbar treemacs swiper counsel imenu-list evil-indent-plus helm-projectile helm-ag-r helm-ag ranger helm spacemacs-theme use-package powerline php-mode evil-surround evil-leader elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
