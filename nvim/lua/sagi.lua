c912a8bec2807db19c7bf65ebe10997c177966dd
sagidana <sagidana1@gmail.com>
2024-07-03 22:36:00 +0300

    Update sagi.lua

--- nvim/lua/sagi.lua
+++ nvim/lua/sagi.lua
@@ -1,5 +1,6 @@
+vim.g.mapleader = " "
 vim.opt.cursorline = true
-vim.g.have_nerd_font = true
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
@@ -9,258 +10,68 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
-require('lazy').setup({ --    :Lazy
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
---   {
---     'nvim-telescope/telescope.nvim',
---     event = 'VimEnter',
---     branch = '0.1.x',
---     dependencies = {
---       'nvim-lua/plenary.nvim',
---       {
---         'nvim-telescope/telescope-fzf-native.nvim',
---         build = 'make',
---         cond = function()
---           return vim.fn.executable 'make' == 1
---         end,
---       },
---       { 'nvim-telescope/telescope-ui-select.nvim' },
--- 
---       -- Useful for getting pretty icons, but requires a Nerd Font.
---       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
---     },
---     config = function()
---       -- [[ Configure Telescope ]]
---       require('telescope').setup {
---         extensions = {
---           ['ui-select'] = {
---             require('telescope.themes').get_dropdown(),
---           },
---         },
---       }
--- 
---       -- Enable Telescope extensions if they are installed
---       pcall(require('telescope').load_extension, 'fzf')
---       pcall(require('telescope').load_extension, 'ui-select')
--- 
---       -- See `:help telescope.builtin`
---       local builtin = require 'telescope.builtin'
---       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
---       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
---       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
---       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
---       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
---       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
---       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
---       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
---       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
---       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
--- 
---       -- Slightly advanced example of overriding default behavior and theme
---       vim.keymap.set('n', '<leader>/', function()
---         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
---         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
---           winblend = 10,
---           previewer = false,
---         })
---       end, { desc = '[/] Fuzzily search in current buffer' })
--- 
---       vim.keymap.set('n', '<leader>s/', function()
---         builtin.live_grep {
---           grep_open_files = true,
---           prompt_title = 'Live Grep in Open Files',
---         }
---       end, { desc = '[S]earch [/] in Open Files' })
--- 
---       -- Shortcut for searching your Neovim configuration files
---       vim.keymap.set('n', '<leader>sn', function()
---         builtin.find_files { cwd = vim.fn.stdpath 'config' }
---       end, { desc = '[S]earch [N]eovim files' })
---     end,
---   },
-  { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
-    },
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-
-          -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          -- map('gx', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-          map('gx', vim.lsp.buf.references, '[G]oto [R]eferences')
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-          -- -- Execute a code action, usually your cursor needs to be on top of an error
-          -- -- or a suggestion from your LSP for this to activate.
-          -- map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+require('lazy').setup(
+{
+    {
+        'nvim-treesitter/nvim-treesitter',
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                }
+            }
         end,
-      })
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      local servers = {
-        clangd = {},
-        gopls = {},
-        pyright = {},
-        rust_analyzer = {},
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              diagnostics = {
-                disable = { 'missing-fields' },
-                globals = { 'vim' },
-              },
-            },
-          },
-        },
-      }
-
-      require('mason').setup()
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-    end,
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
     },
-    config = function()
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
+    'nvim-treesitter/playground',
+    'mg979/vim-visual-multi',
+    -- {
+        -- 'theprimeagen/harpoon',
+        -- dependencies = { 'nvim-lua/plenary.nvim' },
+        -- config = function()
+        -- local mark = require("harpoon.mark")
+        -- local ui = require("harpoon.ui")
 
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
-  },
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-    end,
-  },
-
---    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
---    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-
-}, {
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
+        -- vim.keymap.set("n", "<leader>a", mark.add_file)
+        -- vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+        -- end
+    -- },
+    {
+        'mbbill/undotree',
+        config = function()
+            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        end
     },
-  },
+
+    'neovim/nvim-lspconfig',
+    {
+        'williamboman/mason.nvim',
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        on_attach = function(client, bufnr)
+                            vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+                            vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+                            vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+                        end
+                    })
+                end,
+                -- ["lua_ls"] = function()
+                -- end
+            })
+            vim.diagnostic.config({  -- https://neovim.io/doc/user/diagnostic.html
+                virtual_text = false,
+                signs = false,
+            })
+        end
+    },
 })
-
--- vim: ts=2 sts=2 sw=2 et
