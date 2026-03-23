ae9aefcb790ab9dfb7c7eefd6af1324cf55751a3
Sagi Dana <sagidana1@gmail.com>
2026-03-23 10:15:08 -0400

    update claude sandbox

--- claude_sandbox/run.sh
+++ claude_sandbox/run.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-PROMPT="$*"
+# docker run --rm -it\
+  # -e DISPLAY=$DISPLAY \
+  # -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+  # -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+  # -v /dev/shm:/dev/shm \
+  # -v /tmp/.X11-unix:/tmp/.X11-unix \
+  # -v "$PWD/repo":/repo \
+  # -v "$PWD/home":/home/node \
+  # -w /repo \
+  # claude-sandbox
 
-docker run --rm -it\
+REPO="$1"
+
+docker run --rm -it \
+  --network space-inet-net \
+  --dns 192.168.1.1 \
   -e DISPLAY=$DISPLAY \
   -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
   -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
   -v /dev/shm:/dev/shm \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /mnt/wslg:/mnt/wslg \
-  -v "$PWD/repo":/repo \
+  -v $REPO:/repo \
   -v "$PWD/home":/home/node \
   -w /repo \
   claude-sandbox
